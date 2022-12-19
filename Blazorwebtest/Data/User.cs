58b0a468bf6eacf775222d1bfd8f5e9ee510a17b
Rifat Murtuza <rm.rudro@gmail.com>
2022-12-20 07:33:01 +0900

    First Commit

---
+++ Blazorwebtest/Data/User.cs
@@ -0,0 +1,20 @@
+﻿namespace Blazorwebtest.Data
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string EmailAddress { get; set; }
+        public string Password { get; set; }
+        public string Source { get; set; }
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+        public string LastName { get; set; }
+        public int PubId { get; set; }
+        public DateTime? HireDate { get; set; }
+
+        //public Role Role { get; set; }
+        //public string ConfirmPassword { get; set; }
+        //public string AccessToken { get; set; }
+        //public string RefreshToken { get; set; }
+    }
+}
